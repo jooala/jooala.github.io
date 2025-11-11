1049c7886d65a9c551827d7b16ae241d202ab1c0
jooala <joonas.alanenpaa@gmail.com>
2025-11-11 14:02:55 +0200

    Nextjs fixes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const path = require('path');
 
 module.exports = {
+  output: 'export',
   assetPrefix: isProd ? '/' : '',
   images: {
     unoptimized: true,
